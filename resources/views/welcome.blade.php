5af5da1e37643bf4ab363731847b2522ad9e093d
smithmakario <sisanmi.smith@makarioworks.com>
2026-02-05 13:06:11 +0100

    feat/welcome updated welcome page

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -3,7 +3,7 @@
 <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Platform Multi-Role Login Portal</title>
+    <title>Segun Obadje School for Men - SMSM</title>
     <script src="https://cdn.tailwindcss.com?plugins=forms,container-queries"></script>
     <link href="https://fonts.googleapis.com/css2?family=Inter:wght@300;400;500;600;700;800;900&amp;display=swap" rel="stylesheet">
     <link href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:wght,FILL@100..700,0..1&amp;display=swap" rel="stylesheet">
